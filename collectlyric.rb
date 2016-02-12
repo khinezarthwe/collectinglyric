4efa27ce871e518ebc9b57518d14f3187fcd1f99
khinezarthwe <koalakhine@gmail.com>
2016-02-12 18:40:57 +0900

    adding raw lyric

--- collectlyric.rb
+++ collectlyric.rb
@@ -1,6 +1,13 @@
 require 'rubygems'
+require 'csv'
 require 'lyricfy'
-
+data = CSV.read('data/adddata/song_id,artistname_song_name2.csv')
 fetcher = Lyricfy::Fetcher.new
-song = fetcher.search 'Elliott Smith', 'Placeholder'
-p song.body("<br>")
+data.each do |songdata|
+  begin
+    song = fetcher.search songdata[1],songdata[2]
+    File.open('data/lyricdata/'+songdata[0]+ '.txt', 'w') {|f| f.write(song.body('')) }
+  rescue Exception => e
+    File.open('data/nolyric/'+songdata[0]+ '.txt', 'w') {|f| f.write(e.message) }
+  end
+end
--- testing.rb
+++ testing.rb
@@ -1,19 +1,15 @@
+require 'rubygems'
 require 'csv'
-result_arr = []
+require 'lyricfy'
+fetcher = Lyricfy::Fetcher.new
+data = CSV.read('data/adddata/testdata.csv')
+data.each do |songdata|
+  begin
+    song = fetcher.search songdata[1], songdata[2]
+    File.open('data/2timetest/'+songdata[0]+ '.txt', 'w') {|f| f.write(song.body('')) }
+  rescue Exception => e
+    File.open('data/2timetest/no/'+songdata[0]+ '.txt', 'w') {|f| f.write(e.message) }
+  end
+end
 
-data1 = CSV.read('data/adddata/002.csv',headers:true)
-dataheader = data1.headers
-data = CSV.read('data/adddata/20userdata/User_3_2009.csv')
-dataheader.each do |test|
-  data.each do |check|
-  if test == check[4]
-   result_arr << [[check[4]],check[3],check[5]]
-  end
-  end
-end
-puts result_arr.uniq.count
-CSV.open("data/adddata/output.csv" ,"w") do |csvobject|
-  result_arr.each do |row_arr|
-    csvobject << row_arr
-  end
-end
+#puts song.body # prints lyrics separated by '\n'
